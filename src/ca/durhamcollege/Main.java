02448df4206f3e959b77b7fe24c34cbf36b7f5a1
jacky <jacky.yuan@dcmail.ca>
2020-11-11 10:32:17 -0500

    basic input and output

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,8 +1,22 @@
+/*
+*Author: Jacky Yuan
+* Date: Nov 11, 2020
+*
+ */
+
 package ca.durhamcollege;
+
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+    public static void main(String[] args)
+    {
+        String name = "";
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter your name");
+        name = keyboard.nextLine();
+        System.out.printf("You Entered: %s",name);
+
+	}
 }
