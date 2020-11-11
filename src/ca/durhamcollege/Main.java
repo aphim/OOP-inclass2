c890de1aa7236d514e278f2147bd1d2333e98f9a
jacky <jacky.yuan@dcmail.ca>
2020-11-11 10:40:19 -0500

    input and output variables

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -14,9 +14,15 @@ public class Main {
     {
         String name = "";
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Please enter your name");
+        System.out.print("Please enter your name: ");
         name = keyboard.nextLine();
-        System.out.printf("You Entered: %s",name);
+        float age = 0.0f;
+        System.out.print("Please enter your age: ");
+        age = keyboard.nextFloat();
+        System.out.println();
+        //output
+        System.out.printf("You Entered: %s\n",name);
+        System.out.printf("You Entered: %.2f\n",age);
 
 	}
 }
